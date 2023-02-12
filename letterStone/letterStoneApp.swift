c30df549106ca4abe2eb286cb9875c22c8deb29e
lsy96999 <lsy96999@gmail.com>
2023-02-12 15:25:28 +0900

    Initial Commit

---
+++ letterStone/letterStoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  letterStoneApp.swift
+//  letterStone
+//
+//  Created by N-312 on 2023/02/12.
+//
+
+import SwiftUI
+
+@main
+struct letterStoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
